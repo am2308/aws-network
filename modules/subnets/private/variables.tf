d8162f84183137126f592ce07a26715eb2097f92
am2308 <b110020510@gmail.com>
2022-04-28 22:45:00 +0100

    added private subnet variables

--- modules/subnets/private/variables.tf
+++ modules/subnets/private/variables.tf
@@ -1,1 +1,35 @@
+variable "private_subnet_name" {
+  type = string
+}
 
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_cidrs" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "tags" {
+  type    = map(string)
+}
+
+variable "natgw_ids" {
+  type    = list(string)
+}
+
+variable "vpn_gateway" {
+  type    = string
+}
+
+variable "enable_nat_gateway" {
+  type    = bool
+}
+
+variable "destination_cidr" {
+  type    = string
+}
