6d46e514f57a959e7292db8b6252bcb3c8fa1e99
am2308 <b110020510@gmail.com>
2022-04-28 22:40:50 +0100

    added output variables to pass to other modules

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,1 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.default-vpc.id
+}
 
+output "vpc_cidr" {
+  value = aws_vpc.default-vpc.cidr_block
+}
+
+output "igw_id" {
+  value = var.enable_internet_gateway ? aws_internet_gateway.default-igw["enabled"].id : ""
+}
