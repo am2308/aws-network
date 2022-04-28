c6b2ceaba2dfe58f8fb0307ef26854f11caa079a
am2308 <b110020510@gmail.com>
2022-04-28 22:40:12 +0100

    added variables for vpc creation

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,22 @@
+variable "vpc_name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+}
 
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+}
+
+variable "secondary_cidr_blocks" {
+  description = "List of secondary CIDR blocks to associate with the VPC to extend the IP Address pool"
+  type        = list(string)
+}
+
+variable "tags" {
+  type    = map(string)
+}
+
+variable "enable_internet_gateway" {
+  type    = bool
+}
