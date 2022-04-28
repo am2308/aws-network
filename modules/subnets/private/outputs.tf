9ae1c425c78fdac23d6ac0ef093745c34f3d90be
am2308 <b110020510@gmail.com>
2022-04-28 22:45:37 +0100

    added outputs to pass to other modules

--- modules/subnets/private/outputs.tf
+++ modules/subnets/private/outputs.tf
@@ -1,1 +1,18 @@
+output "private_subnets" {
+  description = "List of Subnet ids"
+  value       = aws_subnet.default-private-subnet.*.id
+}
 
+output "private_route_tables" {
+  value = aws_route_table.default-private-route-table.*.id
+}
+
+output "private_azs" {
+  description = "Subnet AZs"
+  value       = var.azs
+}
+
+output "private_cidrs" {
+  description = "List of cidr_blocks"
+  value       = aws_subnet.default-private-subnet.*.cidr_block
+}
