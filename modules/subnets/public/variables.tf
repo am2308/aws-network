bfcc075d3738cb6790879553475b1281c85fec79
am2308 <b110020510@gmail.com>
2022-04-29 08:59:24 +0100

    added variables for public subnet

--- modules/subnets/public/variables.tf
+++ modules/subnets/public/variables.tf
@@ -1,1 +1,32 @@
+variable "public_subnet_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "igw_id" {
+  type = string
+}
+
+variable "public_cidrs" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "tags" {
+  type    = map(string)
+}
+
+variable "enable_nat_gateway" {
+  type    = bool
+}
+
+variable "destination_cidr" {
+  type    = string
+}
 
