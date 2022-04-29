ce5d556416afa5dd5efd7761f85931203ab31b7d
am2308 <b110020510@gmail.com>
2022-04-29 09:05:43 +0100

    added providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,11 @@
-
+provider "aws" {
+  region = "eu-west-1"
+  version = "~> 4.10.0"
+  default_tags {
+    tags = {
+      Environment     = var.env
+      ManagedBy       = var.managed_by
+      Project         = var.project
+    }
+  }
+}
