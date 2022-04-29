47602db6ebff45520b7c1ae286f471a0b7b18eb6
am2308 <b110020510@gmail.com>
2022-04-29 09:00:18 +0100

    added main.tf for security group

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -1,1 +1,25 @@
+##This resource will create security group to attch with ec2
+resource "aws_security_group" "default-ec2-sg" {
+  name = var.security_group_name
+  vpc_id = var.vpc_id
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = var.ingress_cidr
+  }
+
+  tags = merge(
+    {
+      "Name" = var.security_group_name
+    },
+    var.tags
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
