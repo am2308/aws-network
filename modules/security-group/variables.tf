6ed17ea0049c863231f73d84a23fcbdacf14cf0a
am2308 <b110020510@gmail.com>
2022-04-29 09:01:14 +0100

    added variables for security group

--- modules/security-group/variables.tf
+++ modules/security-group/variables.tf
@@ -1,1 +1,17 @@
+variable "security_group_name" {
+  description = "Name to be used for security group"
+  type        = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "ingress_cidr" {
+  type = list(string)
+}
+
+variable "tags" {
+  type    = map(string)
+}
 
